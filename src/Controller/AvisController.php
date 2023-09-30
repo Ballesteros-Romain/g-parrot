fa2f3ed73f5aa276f5cb4a0e00936d42c7394d71
Ballesteros-Romain <romain.ballesteros@gmail.com>
2023-09-30 18:08:18 +0200

    create moderate avis

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -2,17 +2,58 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Avis;
+use App\Form\AvisFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AvisController extends AbstractController
 {
     #[Route('/avis', name: 'app_avis')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+
+        $avisForm = $this->createForm(AvisFormType::class);
+
+
+        $avisForm->handleRequest($request);
+
+        if ($avisForm->isSubmitted() && $avisForm->isValid()) {
+            $formData = $avisForm->getData();
+            $name = $formData->getName();
+            $commentaire = $formData->getCommentaire();
+            $note = $formData->getNote();
+            $isActive = $formData->getIsActive();
+
+
+            $avis = new Avis();
+            $avis->setName($name);
+            $avis->setCommentaire($commentaire);
+            $avis->setNote($note);
+            $avis->setIsActive($isActive);
+
+            try {
+                $entityManager->persist($avis);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Nous avons pris en compte votre avis');
+                return $this->redirectToRoute('home_');
+            } catch (\Exception $e) {
+                $this->addFlash('danger', 'Une erreur est survenue : ' . $e->getMessage());
+            }
+        }
+
+
         return $this->render('avis/index.html.twig', [
-            'controller_name' => 'AvisController',
+            'avisForm' => $avisForm->createView(),
         ]);
     }
+    
+    public function displayAvis(){
+        
+    }
+
 }
