42c1b8e514aa963e1be1f37cc7be812605c21844
Ballesteros-Romain <romain.ballesteros@gmail.com>
2023-10-01 20:28:54 +0200

    start create moderation clock

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Avis;
 use App\Form\AvisFormType;
+use App\Repository\AvisRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AvisController extends AbstractController
 {
     #[Route('/avis', name: 'app_avis')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, AvisRepository $avisRepository): Response
     {
 
         $avisForm = $this->createForm(AvisFormType::class);
@@ -45,15 +46,15 @@ class AvisController extends AbstractController
                 $this->addFlash('danger', 'Une erreur est survenue : ' . $e->getMessage());
             }
         }
+            $avis = $avisRepository->findAll();
+            $context = compact('avis');
+            $context['avis'] = $avis;
 
 
         return $this->render('avis/index.html.twig', [
             'avisForm' => $avisForm->createView(),
+            'avis' =>$avis,
+            
         ]);
     }
-    
-    public function displayAvis(){
-        
-    }
-
 }
--- src/Controller/HorairesController.php
+++ src/Controller/HorairesController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Horaires;
+use App\Repository\HorairesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HorairesController extends AbstractController
 {
     #[Route('/horaires', name: 'app_horaires')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em, HorairesRepository $horairesRepository): Response
     {
-        return $this->render('horaires/index.html.twig', [
-            'controller_name' => 'HorairesController',
+
+        $horaires = $horairesRepository->findAll();
+        
+        return $this->render('_partials/_footer.html.twig', [
+            'horaires'=>$horaires,
         ]);
     }
 }
