fd44f322f31ea617cfd9a957228fd4c0650b3f0f
Ballesteros-Romain <romain.ballesteros@gmail.com>
2023-10-02 09:37:26 +0200

    display avis on homepage

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -57,4 +57,5 @@ class AvisController extends AbstractController
             
         ]);
     }
+    
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AvisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home_')]
-    public function index(): Response
+    public function index(AvisRepository $avisRepository): Response
     {
+
+        $avis = $avisRepository->findAll();
+        // $context = compact('avis');
+        // $context['avis'] = $avis;
+
+        return $this->render('home/index.html.twig', [
+            'avis' => $avis
+        ]);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
