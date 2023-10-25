cb6463c707f5f78fcb28dcab894ebfce27b96afd
Ballesteros-Romain <romain.ballesteros@gmail.com>
2023-10-25 16:11:36 +0200

    modify 2123 3059

--- migrations/Version20230930153059.php
+++ migrations/Version20230930153059.php
@@ -20,7 +20,7 @@ final class Version20230930153059 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE avis DROP is_active');
+        // $this->addSql('ALTER TABLE avis DROP is_active');
     }
 
     public function down(Schema $schema): void
