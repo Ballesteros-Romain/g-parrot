3e663bf84b363fbc987728dd56bd1a4d14424c20
Ballesteros-Romain <romain.ballesteros@gmail.com>
2023-10-25 16:04:32 +0200

    modify 2928

--- migrations/Version20230929142928.php
+++ migrations/Version20230929142928.php
@@ -20,7 +20,7 @@ final class Version20230929142928 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE avis CHANGE is_active is_active TINYINT(1) NOT NULL');
+        // $this->addSql('ALTER TABLE avis CHANGE is_active is_active TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
