5ec252a2276c951f5d5aaf04650634a57e3b588c
Ballesteros-Romain <romain.ballesteros@gmail.com>
2023-10-27 15:54:16 +0200

    re update contact assert\length

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -60,10 +60,9 @@ class ContactFormType extends AbstractType
                         'pattern' => '/^[0-9\s]+$/',
                         'message' => 'Le numéro de téléphone ne peut contenir que des chiffres et des espaces.',
                     ]),
-                    // new Assert\Length([
-                    //     'min' => 10,
-                    //     'message' => 'Le numéro doit contenir 10 chiffres'
-                    // ])
+                    new Assert\Length([
+                        'min' => 10,
+                    ])
                 ],
             ])
             ->add('message', TextareaType::class, [
